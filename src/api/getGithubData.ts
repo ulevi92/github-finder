8bd6e044bde5617fc3c176a1067987dfe4a82297
Uria <ulevi92@gmail.com>
2022-01-23 02:21:43 +0200

    fixed/ cleared github token. Can do 10 search/minute

--- src/api/getGithubData.ts
+++ src/api/getGithubData.ts
@@ -2,18 +2,13 @@ import {} from "react-router-dom";
 import { GetDataProps, GetUserReposType, GetUserType } from "./getGithubTypes";
 
 export const gitUrl = process.env.REACT_APP_GITHUB_URL;
-export const gitToken = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const getUsersGithubData = async (text: string) => {
   const params = new URLSearchParams({
     q: text,
   });
 
-  const res = await fetch(`${gitUrl}/search/users?${params}`, {
-    headers: {
-      Authorization: `token ${gitToken}`,
-    },
-  });
+  const res = await fetch(`${gitUrl}/search/users?${params}`);
 
   const data: GetDataProps = await res.json();
 
@@ -21,11 +16,7 @@ export const getUsersGithubData = async (text: string) => {
 };
 
 export const getUserGithubData = async (login: string) => {
-  const res = await fetch(`${gitUrl}/users/${login}`, {
-    headers: {
-      Authorization: `token ${gitToken}`,
-    },
-  });
+  const res = await fetch(`${gitUrl}/users/${login}`);
 
   if (res.status === 404) {
     window.location.replace("/notfound");
@@ -42,11 +33,7 @@ export const getUserGithubRepos = async (login: string) => {
     per_page: "10",
   });
 
-  const res = await fetch(`${gitUrl}/users/${login}/repos?${params}`, {
-    headers: {
-      Authorization: `token ${gitToken}`,
-    },
-  });
+  const res = await fetch(`${gitUrl}/users/${login}/repos?${params}`);
 
   const data: GetUserReposType[] = await res.json();
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,17 +3,19 @@ const About = () => {
     <>
       <h1 className='text-6xl mb-4'>Github Finder</h1>
       <p className='mb-4 text-2xl font-light'>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {" "}
-          React Front To Back
-        </a>{" "}
-        Udemy course by
-        <strong>
-          <a href='https://traversymedia.com'> Brad Traversy</a>
-        </strong>
-        .
+        A React app to search GitHub profiles and see profile details.
+      </p>
+
+      <p className='text-1xl'>
+        Visit my
+        <a
+          href='https://github.com/ulevi92'
+          target='_blank'
+          rel='noreferrer'
+          className='hover:text-sky-500'
+        >
+          {` Github`}
+        </a>
       </p>
     </>
   );
